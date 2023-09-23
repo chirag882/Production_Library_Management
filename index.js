9fbebc2526a524f479da35fb617ca45ff3ccd77c
Chirag Ailani <chiragailani35@gmail.com>
2023-09-24 03:14:57 +0530

    Final

--- index.js
+++ index.js
@@ -12,11 +12,14 @@ connectDatabase();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 // import routes
 const Routes = require("./routes/Route");
 app.use("/", Routes);
-
+app.use("*", function (req, res) {
+  res.sendfile(path.join(__dirname, "./client/build/index.html"));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
